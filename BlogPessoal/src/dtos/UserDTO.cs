42bffea6568362c7a27a8348e81568101f289884
cateldev <catelmatt@hotmail.com>
2022-04-29 10:50:03 -0300

    @cateldev UI Update [29/04/22]

--- BlogPessoal/src/dtos/UserDTO.cs
+++ BlogPessoal/src/dtos/UserDTO.cs
@@ -2,6 +2,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BlogPessoal.src.dtos
 {
+
+///<summary>
+///<para>Resumo: Classe espelho para criar um novo usuário</para>
+///<para> Criado por: Gustavo Boaz</para>
+///<para>Versão: 1.0</para>
+///<para>Data: 29/04/2022</para>
+///</summary>
     public class NewUserDTO
     {
         [Required, StringLength(50)]
@@ -26,5 +33,20 @@ namespace BlogPessoal.src.dtos
 
     public class UpdateUserDTO
     {
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+
+        [Required, StringLength(30)]
+
+        public string Password { get; set; }
+
+        public string Photo { get; set; }
+
+         public UpdateUserDTO(string name, string password, string photo)
+        {
+            Name = name;
+            Password = password;
+            Photo = photo;
+        }
     }
 }
