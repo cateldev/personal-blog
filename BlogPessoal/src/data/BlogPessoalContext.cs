959bc2cfa66025b15ffd20bd03b23cfddcd324e2
cateldev <catelmatt@hotmail.com>
2022-05-18 15:12:15 -0300

    @cateldev [Task: Context Documentation] {18/5/2022}

--- BlogPessoal/src/data/BlogPessoalContext.cs
+++ BlogPessoal/src/data/BlogPessoalContext.cs
@@ -3,6 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BlogPessoal.src.data
 {
+    /// <summary>
+    /// <para>Resume: Class responsible for data base Blog context</para>
+    /// <para>Created by: Matheus Catel</para>
+    /// <para>Version: 1.0</para>
+    /// <para>Date: 2022-05-12</para>
+    /// </summary>
+
     public class BlogPessoalContext : DbContext
     {
         public DbSet<UserModel> Users { get; set; }
