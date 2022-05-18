11a33daf486f69124c556c9c109b095b7e142041
cateldev <catelmatt@hotmail.com>
2022-05-18 14:59:42 -0300

    @cateldev [Task: ThemeController Documentation] {18/5/2022}

--- BlogPessoal/src/controllers/ThemeController.cs
+++ BlogPessoal/src/controllers/ThemeController.cs
@@ -13,17 +13,40 @@ namespace BlogPessoal.src.controladores
     public class ThemeController : ControllerBase
     {
         #region Attributes
+
         private readonly ITheme _repository;
+
         #endregion Attributes
 
         #region Constructors
+
         public ThemeController(ITheme repository)
+
         {
         _repository = repository;
         }
+
         #endregion Constructors
 
         #region Métodos
+
+        /// <summary>
+        /// Create a new theme
+        /// </summary>
+        /// <param name="theme">ThemeRegisterDTO</param>
+        /// <returns>IActionResult</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /api/Theme
+        ///     {
+        ///        "description": "C#"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Returns the newly created theme</response>
+        /// <response code="400">Error in request</response>
+
         [HttpPost]
         [Authorize]
         public async Task<ActionResult> NewThemeAsync([FromBody] NewThemeDTO theme)
@@ -35,6 +58,24 @@ namespace BlogPessoal.src.controladores
             return Created($"api/Themes", theme);
         }
 
+        /// <summary>
+        /// Update theme by id
+        /// </summary>
+        /// <param name="theme">ThemeRegisterDTO</param>
+        /// <returns>IActionResult</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     PUT /api/Theme
+        ///     {
+        ///        "description": "Python"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">Theme updated</response>
+        /// <response code="400">Error in request</response>
+        /// <response code="404">Theme not found</response>
+
         [HttpPut]
         [Authorize(Roles = "ADMINISTRATOR")]
         public async Task<ActionResult> UpdateTheme([FromBody] UpdateThemeDTO theme)
@@ -44,6 +85,14 @@ namespace BlogPessoal.src.controladores
             return Ok(theme);
         }
 
+        /// <summary>
+        /// Delete theme by id
+        /// </summary>
+        /// <param name="idTheme">int</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="200">Theme deleted</response>
+        /// <response code="404">Theme not found</response>
+
         [HttpDelete("delete/{idTheme}")]
         [Authorize(Roles = "ADMINISTRATOR")]
         public async Task<ActionResult> DeleteTheme([FromRoute] int idTheme)
@@ -51,6 +100,13 @@ namespace BlogPessoal.src.controladores
             await _repository.DeleteThemeAsync(idTheme);
             return NoContent();
         }
+
+        /// <summary>
+        /// Get all themes
+        /// </summary>
+        /// <returns>IActionResult</returns>
+        /// <response code="200">Returns all themes</response>
+        /// <response code="204">No content</response>
 
         [HttpGet]
         [Authorize]
@@ -60,6 +116,14 @@ namespace BlogPessoal.src.controladores
             if (list.Count  < 1) return NoContent();
             return Ok(list);
         }
+
+        /// <summary>
+        /// Get theme by id
+        /// </summary>
+        /// <param name="idTheme">int</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="200">Returns the theme</response>
+        /// <response code="404">Theme not found</response>
 
         [HttpGet("id/{idTheme}")]
         [Authorize]
@@ -71,6 +135,14 @@ namespace BlogPessoal.src.controladores
             return Ok(theme);
 
         }
+
+        /// <summary>
+        /// Get themes by description
+        /// </summary>
+        /// <param name="descriptionTheme">string</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="200">Returns the theme</response>
+        /// <response code="204">Theme no content</response>
 
         [HttpGet("search")]
         [Authorize]
